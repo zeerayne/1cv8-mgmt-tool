0c185cdeb59fa2a9cc548475f9f17659c5d23433
Ilya Bogdanov <zeerayne1337@gmail.com>
2022-06-06 19:55:25 +0300

    Add more possible infobase job result mocks

--- conftest.py
+++ conftest.py
@@ -1,7 +1,12 @@
 import os
+import random
+
 import pytest
 
 from core import types as core_types
+
+
+random.seed(0)
 
 
 def pytest_generate_tests(metafunc):
@@ -63,4 +68,81 @@ def mixed_backup_result(infobases):
         infobase_name=ib,
         succeeded=(succeeded := not succeeded),
         backup_filename=f'./{ib}.testbackup' if succeeded else ''
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def success_maintenance_result(infobases):
+    return [core_types.InfoBaseMaintenanceTaskResult(
+        infobase_name=ib,
+        succeeded=True
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def failed_maintenance_result(infobases):
+    return [core_types.InfoBaseMaintenanceTaskResult(
+        infobase_name=ib,
+        succeeded=False,
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def mixed_maintenance_result(infobases):
+    succeeded = True
+    return [core_types.InfoBaseMaintenanceTaskResult(
+        infobase_name=ib,
+        succeeded=(succeeded := not succeeded)
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def success_update_result(infobases):
+    return [core_types.InfoBaseUpdateTaskResult(
+        infobase_name=ib,
+        succeeded=True
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def failed_update_result(infobases):
+    return [core_types.InfoBaseUpdateTaskResult(
+        infobase_name=ib,
+        succeeded=False,
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def mixed_update_result(infobases):
+    succeeded = True
+    return [core_types.InfoBaseUpdateTaskResult(
+        infobase_name=ib,
+        succeeded=(succeeded := not succeeded)
+    ) for ib in infobases]   
+
+
+@pytest.fixture
+def success_aws_result(infobases):
+    return [core_types.InfoBaseAWSUploadTaskResult(
+        infobase_name=ib,
+        succeeded=True,
+        upload_size=random.randint(1000, 1000 ** 2)
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def failed_aws_result(infobases):
+    return [core_types.InfoBaseAWSUploadTaskResult(
+        infobase_name=ib,
+        succeeded=False
+    ) for ib in infobases]
+
+
+@pytest.fixture
+def mixed_aws_result(infobases):
+    succeeded = True
+    return [core_types.InfoBaseAWSUploadTaskResult(
+        infobase_name=ib,
+        succeeded=(succeeded := not succeeded),
+        upload_size=random.randint(1000, 1000 ** 2) if succeeded else 0
     ) for ib in infobases]
