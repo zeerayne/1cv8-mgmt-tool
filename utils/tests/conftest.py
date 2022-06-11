9b174e4374d3f4f9d85be7b9d9c26e9dfa6a496a
Ilya Bogdanov <zeerayne1337@gmail.com>
2022-06-11 19:14:39 +0300

    `utils.tests.conftest` improvements

--- utils/tests/conftest.py
+++ utils/tests/conftest.py
@@ -1,25 +1,26 @@
+from unittest.mock import MagicMock, Mock
+
 import pytest
 from pytest_mock import MockerFixture
 
 
 @pytest.fixture
 def mock_smtp(mocker: MockerFixture):
-    return mocker.patch('smtplib.SMTP.connect', return_value=(220, ''))
+    return mocker.patch('smtplib.SMTP', return_value=MagicMock())
 
 
 @pytest.fixture
-def mock_smtp_login(mocker: MockerFixture):
-    return mocker.patch('smtplib.SMTP.login', return_value=None)
+def mock_smtp_login(mock_smtp):
+    smtp_login_mock = Mock()
+    type(mock_smtp.return_value.__enter__.return_value).login = smtp_login_mock
+    return smtp_login_mock
 
 
 @pytest.fixture
-def mock_smtp_sendmail(mocker: MockerFixture):
-    return mocker.patch('smtplib.SMTP.sendmail', return_value=None)
-
-
-@pytest.fixture
-def mock_smtp_quit(mocker: MockerFixture):
-    return mocker.patch('smtplib.SMTP.quit', return_value=None)
+def mock_smtp_sendmail(mock_smtp):
+    smtp_sendmail_mock = Mock()
+    type(mock_smtp.return_value.__enter__.return_value).sendmail = smtp_sendmail_mock
+    return smtp_sendmail_mock
 
 
 @pytest.fixture
