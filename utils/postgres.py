46016850b402236fce45bdea0eb5bd8754b1871b
Ilya Bogdanov <zeerayne1337@gmail.com>
2022-06-14 22:14:36 +0300

    Add postgres util: `prepare_postgres_connection_vars`

--- utils/postgres.py
+++ utils/postgres.py
@@ -1,5 +1,7 @@
 from typing import Tuple
 
+from conf import settings
+
 
 POSTGRES_DEFAULT_PORT = '5432'
 
@@ -11,3 +13,15 @@ def get_postgres_host_and_port(db_server_string: str) -> Tuple[str, str]:
     except IndexError:
         db_port = POSTGRES_DEFAULT_PORT
     return db_host, db_port
+
+
+def prepare_postgres_connection_vars(db_server: str, dbms: str, db_name: str, db_user: str) -> Tuple[str, str, str, str, str]:
+    if dbms.lower() != 'PostgreSQL'.lower():
+        raise ValueError(f'PostgreSQL connection can not be set up for {dbms} DBMS')
+    db_user_string = f'{db_user}@{db_server}'
+    db_host, db_port = get_postgres_host_and_port(db_server)
+    try:
+        db_pwd = settings.PG_CREDENTIALS[db_user_string]
+    except KeyError:
+        raise KeyError(f'PostgreSQL: password not found for user {db_user_string}')
+    return db_host, db_port, db_name, db_user, db_pwd
