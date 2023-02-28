ddbbfb50e43854626c580873582db057de04db07
Ilya Bogdanov <zeerayne1337@gmail.com>
2023-02-28 23:05:08 +0300

    RAGENT_HOME is env variable now

--- docker/ragent-entrypoint.sh
+++ docker/ragent-entrypoint.sh
@@ -5,8 +5,9 @@ export PATH="$PATH:/opt/1cv8/x86_64/$(ls /opt/1cv8/x86_64)"
 
 echo "IP: `awk 'END{print $1}' /etc/hosts`"
 echo "PATH: $PATH"
+echo "RAGENT_HOME: $RAGENT_HOME"
 echo "RAGENT_PORT: $RAGENT_PORT"
 echo "RAGENT_REGPORT: $RAGENT_REGPORT"
 echo "RAGENT_PORTRANGE: $RAGENT_PORTRANGE"
 
-ragent -port $RAGENT_PORT -regport $RAGENT_REGPORT -range $RAGENT_PORTRANGE -d /home/1c
+ragent -port $RAGENT_PORT -regport $RAGENT_REGPORT -range $RAGENT_PORTRANGE -d $RAGENT_HOME
